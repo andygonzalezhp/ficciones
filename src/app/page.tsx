0840722d4c954aa21088fbc7eb003e1f6eb3fb85
andygonzalezhp <ag22by@fsu.edu>
2025-09-14 18:49:28 -0400

    fix: use <Link> for internal nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-[56px] sm:text-[72px] leading-none mb-6">ficciones.net</h1>
+      <h1 className="text-[56px] sm:text-[72px] leading-none mb-6 select-text">
+        ficciones.net
+      </h1>
 
       <ul className="space-y-4 text-[40px] sm:text-[52px] leading-[1.05]">
         <li>
-          <a href="/dreaming" className="hover:italic">dreaming</a>
+          <Link href="/dreaming" prefetch={false} className="hover:italic">
+            dreaming
+          </Link>
         </li>
         <li>
-          <a href="/impression" className="hover:italic">impression</a>
+          <Link href="/impression" prefetch={false} className="hover:italic">
+            impression
+          </Link>
         </li>
       </ul>
     </div>
